475760e3b29a340d13b3694860d03c3e30080c00
Tom <tomarrowtomarrow@hotmail.com>
2026-01-24 17:16:40 +0100

    retry if challenge goes bad

--- JKClient/JKClient.cs
+++ JKClient/JKClient.cs
@@ -1389,6 +1389,12 @@ namespace JKClient {
 						this.infoRequestTime = -9999;
 						this.connectPacketCount = 0;
 						this.Status = ConnectionStatus.Connecting;
+					} else if (this.Status == ConnectionStatus.Challenging && s.Contains("No or bad challenge for address."))
+					{
+						this.connectTime = -9999;
+						this.infoRequestTime = -9999;
+						this.connectPacketCount = 0;
+						this.Status = ConnectionStatus.Connecting;
 					}
 					var cmd = new Command(new string []{ "print", s });
 					this.ServerCommandExecuted?.Invoke(new CommandEventArgs(cmd, -1));
